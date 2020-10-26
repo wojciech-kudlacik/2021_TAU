4bf8ba5132971009769488f3a7e9a97ba1122c71
Wojciech Kudlacik <kudlacik.wojciech@gmail.com>
2020-10-26 14:31:21 +0100

    add animal method

--- src/zoo/Main.java
+++ src/zoo/Main.java
@@ -1,5 +1,26 @@
 package zoo;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
+	static ArrayList<String> animals = new ArrayList<String>();
+	
+	public static void main(String[] args) {
+		
+		Scanner inputScanner = new Scanner(System.in);
+		
+		while(true) {
+			
+			System.out.println("Enter animal name: ");
+			String animal = inputScanner.nextLine();
+			addAnimal(animal);
+		}
+		
+	}
+	
+	private static void addAnimal(String animal) {
+		animals.add(animal);
+	}
 }
