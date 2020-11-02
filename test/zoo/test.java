6894e0eee849a495956eb103709d00a62faae5cd
Wojciech Kudlacik <kudlacik.wojciech@gmail.com>
2020-11-02 20:07:16 +0100

    added testAll

--- test/zoo/test.java
+++ test/zoo/test.java
@@ -30,12 +30,30 @@ class test {
 	}
 	
 	@Test
-	public void testListA() {
+	public void testList() {
 		String[] expectedList = {"Leon", "Felix", "Dumbo"};
 		String[] actualList = main.animals.toArray(new String[0]);
 		assertArrayEquals(expectedList, actualList);
 		
 	}
 	
+	@Test void testAll() {
+		String expectedValue1 = "Leon";
+		String actualValue1 = main.animals.get(0);
+		
+		String expectedValue2 = "Felix";
+		String actualValue2 = main.animals.get(1);
+		
+		assertAll("Should return animals at their specific indexes: ",
+				() -> assertEquals(expectedValue1, actualValue1),
+				() -> assertEquals(expectedValue2, actualValue2)
+			);
+	}
+
+//  How an unhandled exception might be dealt with: 	
+//	@Test
+//	public void testDeleteAnimalException() {
+//		assertThrows(IndexOutOfBoundsException.class, () -> main.deleteAnimal(4));
+//	}
 
 }
