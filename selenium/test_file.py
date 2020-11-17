57bbe04c567414fd3663fc4396862f08d67a6e17
Wojciech Kudlacik <kudlacik.wojciech@gmail.com>
2020-11-17 15:39:34 +0100

    requirements.txt and fix to first file added

--- selenium/test_file.py
+++ selenium/test_file.py
@@ -5,4 +5,4 @@ driver = webdriver.Chrome()
 driver.get("https://www.nytimes.com")
 headlines = driver.find_elements_by_class_name("story-heading")
 for headline in headlines:
-    print(headline.text.strip()
+    print(headline.text.strip())
