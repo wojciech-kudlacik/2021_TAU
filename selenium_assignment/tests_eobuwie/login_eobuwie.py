3a4ab3f557ab3c1f4e44842568f571b89cc3dea1
Wojciech Kudlacik <kudlacik.wojciech@gmail.com>
2020-11-20 17:06:30 +0100

    commented login tests

--- selenium_assignment/tests_eobuwie/login_eobuwie.py
+++ selenium_assignment/tests_eobuwie/login_eobuwie.py
@@ -5,10 +5,17 @@ with open('../config.json', 'r') as file:
 
 
 def login_to_eobuwie(driver):
+    # go to the site
     driver.get("https://www.eobuwie.com.pl/")
+
+    # close gdpr pop-up
     driver.find_element_by_xpath('//*[@id="top"]/body/div[6]/div/div/div/section/button[2]').click()
+
+    # click on login button
     driver.find_element_by_xpath('//*[@id="top"]/body/header/div[3]/div/div/div[2]/div/a[7]').click()
     time.sleep(5)
+    
+    # submit credentials
     driver.find_element_by_xpath('//*[@id="login[username]"]').send_keys(config['email'])
     driver.find_element_by_xpath('//*[@id="login[password]"]').send_keys(config['password'])
     driver.find_element_by_xpath('//*[@id="login-form"]/div[1]/button').click()
--- selenium_assignment/tests_steam/login_steam.py
+++ selenium_assignment/tests_steam/login_steam.py
@@ -5,8 +5,13 @@ with open('../config.json', 'r') as file:
 
 def login_to_steam(driver):
 
+    # go to the site
     driver.get("https://store.steampowered.com/")
+
+    # click on the login button
     driver.find_element_by_xpath('//*[@id="global_action_menu"]/a').click()
+
+    # submit credentials
     driver.find_element_by_id("input_username").send_keys(config['username'])
     driver.find_element_by_id("input_password").send_keys(config['password'])
     driver.find_element_by_xpath('//*[@id="login_btn_signin"]/button').click()
