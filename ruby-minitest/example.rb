02114eb67fe9280a2e269ec88d35dc05b917b8d2
Wojciech Kudlacik <kudlacik.wojciech@gmail.com>
2021-01-17 18:00:58 +0100

    added some example code

--- ruby-minitest/example.rb
+++ ruby-minitest/example.rb
@@ -2,4 +2,8 @@ class Example
     def hello_world
         "hello world"
     end
+
+    def add_numbers a,b
+      a + b 
+    end
 end
--- ruby-minitest/example_test.rb
+++ ruby-minitest/example_test.rb
@@ -2,9 +2,18 @@ require 'minitest/autorun'
 require_relative "example"
 
 class ExampleTest < Minitest::Test
+    
+    def setup
+        @example = Example.new
+    end
+
     def test_ask_returns_string
-        example = Example.new
-        assert example.hello_world == "hello world"
+        assert @example.hello_world == "hello world"
+    end
+
+    def test_ask_returns_number
+        result = @example.add_numbers(2, 2)
+        assert_equal 4, result
     end
 
 end
